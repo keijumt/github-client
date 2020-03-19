3512618534cdf778a87e4a485fb38e3715898210
keijumt <keijumt@gmail.com>
2020-03-19 21:53:37 +0900

    enable kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+buildscript {
+    val kotlinVersion by extra { "1.3.70" }
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.6.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+task("clean", Delete::class) {
+    delete = setOf(rootProject.buildDir)
+}
