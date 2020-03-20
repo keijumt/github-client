68f40a3a46554c549a23720de3411bb1cb42c419
keijumt <keijumt@gmail.com>
2020-03-20 14:07:32 +0900

    Add const

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -1,15 +1,14 @@
-
 object Version {
-    val targetSdkVersion = 29
-    val compileSdkVersion = 29
-    val minSdkVersion = 21
+    const val targetSdkVersion = 29
+    const val compileSdkVersion = 29
+    const val minSdkVersion = 21
 
-    private val versionMajor = 1
-    private val versionMinor = 1
-    private val versionPatch = 0
-    private val versionOffset = 0
-    val versionCode =
+    private const val versionMajor = 1
+    private const val versionMinor = 1
+    private const val versionPatch = 0
+    private const val versionOffset = 0
+    const val versionCode =
         ((1 + versionMajor) * 10000 + versionMinor * 100 + versionPatch) * 100 + versionOffset
 
-    val versionName = "$versionMajor.$versionMinor.$versionPatch"
+    const val versionName = "$versionMajor.$versionMinor.$versionPatch"
 }
