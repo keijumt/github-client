de416d00f3bffc5d2132d6e9afd8d0b39bb9f154
keijumt <keijumt@gmail.com>
2020-03-20 17:28:58 +0900

    Support change toolbar state when changed navigation destination

--- app/src/main/java/keijumt/githubclient/MainActivity.kt
+++ app/src/main/java/keijumt/githubclient/MainActivity.kt
@@ -1,12 +1,62 @@
 package keijumt.githubclient
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import keijumt.githubclient.databinding.ActivityMainBinding
+import keijumt.githubclient.ui.PageConfiguration
 
 class MainActivity : AppCompatActivity() {
 
+    private val navController: NavController by lazy {
+        findNavController(R.id.root_nav_host_fragment)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            onDestinationChange(destination)
+        }
+
+        // TODO: check StatusBarColors
+    }
+
+    private fun onDestinationChange(destination: NavDestination) {
+        val config = PageConfiguration.getConfiguration(destination.id)
+
+        if (config.hideToolbar) {
+            supportActionBar?.hide()
+        } else {
+            supportActionBar?.show()
+        }
+
+        if (!config.hasTitle) {
+            supportActionBar?.title = ""
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(config.displayHomeAsUpEnabled)
+
+        // TODO: check BottomNavigation
+    }
+
+    private fun isNightMode(context: Context): Boolean {
+        return when (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> true
+            Configuration.UI_MODE_NIGHT_NO -> false
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> false
+            else -> false
+        }
     }
 }
---
+++ app/src/main/java/keijumt/githubclient/ui/PageConfiguration.kt
@@ -0,0 +1,25 @@
+package keijumt.githubclient.ui
+
+import androidx.annotation.IdRes
+import keijumt.githubclient.R
+
+enum class PageConfiguration(
+    val id: Int,
+    val hasTitle: Boolean = true,
+    val hideToolbar: Boolean = false,
+    val displayHomeAsUpEnabled: Boolean = false,
+    val hideBottomNavigation: Boolean = false
+) {
+    Launcher(
+       R.id.launcher,
+        hasTitle = false,
+        hideToolbar = true
+    ),
+    Other(0);
+
+    companion object {
+        fun getConfiguration(@IdRes id: Int): PageConfiguration {
+            return enumValues<PageConfiguration>().firstOrNull { it.id == id } ?: Other
+        }
+    }
+}
